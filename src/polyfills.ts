82eba267ac4c6a9c86fbd8d096464b2fd60410b3
Milen Ivanov <m.ivanov@email.com>
2020-05-04 18:54:56 +0300

    enabled zone

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -55,6 +55,8 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
+(window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
+(window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
