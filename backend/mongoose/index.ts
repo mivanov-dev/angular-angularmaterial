2ee7b03b7254382a15194f4760c1925ed45420fa
Milen <m.ivanov@email.com>
2020-07-22 20:40:14 +0300

    fixed method 'dropModels'

--- backend/mongoose/index.ts
+++ backend/mongoose/index.ts
@@ -4,15 +4,16 @@ import * as bluebird from 'bluebird';
 import { User, UserImage } from './models';
 import { log } from '../logger';
 
-( mongoose as any).Promise = bluebird.Promise;
+(mongoose as any).Promise = bluebird.Promise;
 mongoose.set('debug', false);
 
 export function database(uri: string): void {
 
     const connection = mongoose.connection;
-    connection.once('open', async () => {
+    connection.on('open', async () => {
 
         await connection.dropDatabase(err => console.log('mongoose:drop database'));
+
         dropModels();
 
     });
@@ -20,29 +21,36 @@ export function database(uri: string): void {
     connection.on('connecting', () => log.info('mongoose:connecting'));
     connection.on('error', err => log.error('mongoose:error'));
     connection.on('disconnected', () => log.warn('mongoose:disconnected'));
-    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 
 }
 
-function dropModels(): void {
+async function dropModels(): Promise<void> {
 
     const dbModels = [User, UserImage];
-    dbModels.forEach(e => {
 
-        const model = e;
-        model.db.db.listCollections({ name: model.collection.name })
-            .toArray()
-            .then((res: any[]) => {
+    try {
 
-                if (res.length !== 0) {
-                    model.collection.drop();
-                }
+        for (const model of dbModels) {
 
-            })
-            .catch((error) => {
-                log.error(`mongoose:drop models: ${JSON.stringify(error)}`);
-            });
+            const list = await model.db.db.listCollections({
+                name: model.name
+            }).toArray();
 
-    });
+            if (list.length !== 0) {
+                await model.collection.drop();
+            } else {
+                log.warn(`mongoose:dropModels: collection ${model.collection.name} does not exist`);
+            }
+        }
+
+    }
+    catch (error) {
+        log.error(`mongoose:dropModels: ${JSON.stringify(error)}`);
+    }
 
 }
