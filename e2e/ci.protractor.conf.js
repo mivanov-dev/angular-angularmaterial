a3929276dbe0e23a35ef9f28b0a368c4256ca8b4
Milen <m.ivanov@email.com>
2020-08-15 19:40:51 +0300

    added ci.protractor.conf.js for CI

---
+++ e2e/ci.protractor.conf.js
@@ -0,0 +1,51 @@
+// @ts-check
+// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+/**
+ * @type { import("protractor").Config }
+ */
+exports.config = {
+    allScriptsTimeout: 11000,
+    specs: ['./src/features/**/*.feature'],
+    capabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+            'args': [
+                '--disable-translate',
+                '--disable-extensions',
+                '--remote-debugging-port=9223',
+                '--no-sandbox',
+                '--headless',
+                '--disable-gpu'
+            ]
+        }
+    },
+    directConnect: true,
+    baseUrl: 'http://localhost:4200/',
+    framework: 'custom',
+    frameworkPath: require.resolve('protractor-cucumber-framework'),
+    cucumberOpts: {
+        require: ['./src/steps/**/*.steps.ts'],
+        format: [
+            `json:${__dirname}\\report\\cucumber.json`
+        ],
+    },
+    onPrepare() {
+        require('ts-node').register({
+            project: require('path').join(__dirname, './tsconfig.json')
+        })
+    },
+    onComplete() {
+        var reporter = require('cucumber-html-reporter');
+        var options = {
+            theme: 'bootstrap',
+            jsonFile: `${__dirname}\\report\\cucumber.json`,
+            output: `${__dirname}\\report\\cucumber.html`,
+            reportSuiteAsScenarios: true,
+            scenarioTimestamp: true,
+            launchReport: true,
+        };
+        reporter.generate(options);
+    }
+};
