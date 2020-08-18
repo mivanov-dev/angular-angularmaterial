4deb762d625d0df1beb5fec6ae0350c5cdfc5768
Milen <m.ivanov@email.com>
2020-08-18 17:30:15 +0300

    .

--- e2e/ci.protractor.conf.js
+++ e2e/ci.protractor.conf.js
@@ -22,7 +22,7 @@ exports.config = {
             ]
         }
     },
-    directConnect: false,
+    directConnect: true,
     baseUrl: 'http://localhost:4200/',
     framework: 'custom',
     frameworkPath: require.resolve('protractor-cucumber-framework'),
