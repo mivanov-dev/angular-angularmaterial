23e59416aedbc45dd78aa9c6824857af2fc6c5db
Milen <m.ivanov@email.com>
2020-09-15 16:35:08 +0300

    prestructured server.ts & server folder

--- server.ts
+++ server.ts
@@ -1,39 +1,7 @@
 import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
 import 'core-js';
-import * as http from 'http';
 // custom
-import { App } from './backend';
-import { log } from './backend/logger';
-
-export const { app } = App.getInstance();
-
-function run(): void {
-
-  function onError(error: any): void {
-    log.error(`server:error ${JSON.stringify(error)}`);
-  }
-
-  function onListening(protocol: string, host: string, port: string): void {
-    log.info(`Node Express server listening on ${protocol}://${host}:${port}`);
-  }
-
-  // Start up the Node server
-  const httpServer = http.createServer(app);
-  httpServer.listen(app.get('httpPort'));
-  httpServer.on('listening', () => onListening('http', app.get('host'), app.get('httpPort')));
-  httpServer.on('error', err => onError);
-
-}
-
-// Webpack will replace 'require' with '__webpack_require__'
-// '__non_webpack_require__' is a proxy to Node 'require'
-// The below code is to ensure that the server is run only when not requiring the bundle.
-declare const __non_webpack_require__: NodeRequire;
-const mainModule = __non_webpack_require__.main;
-const moduleFilename = (mainModule && mainModule.filename) || '';
-if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
-  run();
-}
+import './server/main';
 
 export * from './src/main.server';
--- server/main.ts
+++ server/main.ts
@@ -9,6 +9,7 @@ import * as cookieParser from 'cookie-parser';
 import * as expressSession from 'express-session';
 import * as passport from 'passport';
 import * as cors from 'cors';
+import * as http from 'http';
 import 'express-validator';
 import * as _ from 'lodash';
 const ms = require('ms');
@@ -34,7 +35,7 @@ global.Typed = window.Typed;
 // @ts-ignore
 global.Image = window.Image;
 
-export class App {
+class App {
 
     private static instance: App;
 
@@ -105,3 +106,33 @@ export class App {
     }
 
 }
+
+export const { app } = App.getInstance();
+
+function run(): void {
+
+    function onError(error: any): void {
+      log.error(`server:error ${JSON.stringify(error)}`);
+    }
+
+    function onListening(protocol: string, host: string, port: string): void {
+      log.info(`Node Express server listening on ${protocol}://${host}:${port}`);
+    }
+
+    // Start up the Node server
+    const httpServer = http.createServer(app);
+    httpServer.listen(app.get('httpPort'));
+    httpServer.on('listening', () => onListening('http', app.get('host'), app.get('httpPort')));
+    httpServer.on('error', err => onError);
+
+  }
+
+  // Webpack will replace 'require' with '__webpack_require__'
+  // '__non_webpack_require__' is a proxy to Node 'require'
+  // The below code is to ensure that the server is run only when not requiring the bundle.
+declare const __non_webpack_require__: NodeRequire;
+const mainModule = __non_webpack_require__.main;
+const moduleFilename = (mainModule && mainModule.filename) || '';
+if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
+    run();
+  }
